6a0d61746f972078ec64967229217ad1bed66bd7
Ryan Tracey <ryan.tracey@gmail.com>
2024-11-18 13:38:06 +0100

    Fixup

--- cv/latex/make_n_serve.sh
+++ cv/latex/make_n_serve.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-pdflatex cv.tex
+
+if [[ -x $(which tectonic) ]]; then
+	echo "Running tectonic"
+	tectonic cv.tex
+elif [[ -x $(which pdflatex) ]]; then
+	echo "Running pdflatex"
+	pdflatex cv.tex
+else
+	echo "No compilers found"
+	exit 1
+fi
+
 python3 -m http.server --directory . 8080
